90267f94da42a318f2711a5cdb68160987e93415
Wenbo Jing <65749925+WenboJing1998@users.noreply.github.com>
2023-01-28 17:11:22 -0500

    Add files via upload

--- povertydatacode.r
+++ povertydatacode.r
@@ -12,6 +12,8 @@ formula <- as.formula(Access.to.electricity + Access.to.drinking.water + Surviva
     School.enrollment ~ Corrup + Deaths.in.violence.per.mille + Democracy + Education.expenditure.percentage + 
     Gini.index + Health.expenditure.percentage + Logistics.performance.index + Pop.dens + Production.index + 
     Rural.pop + Temperature.change + Unemployment.rate)
+
+# Method: Multivariate RE-EM tree
 #
 # 1 SE-based tree using 10-fold CV and surrogate split. Note: plot is edited to add node labels and remove extraneous numbers.
 #
@@ -91,4 +93,129 @@ gglist_treenodes <- list(ggplot(data = world) +
                         )
 
 
+#
+# Method: MRT
+#
 
+#
+# Shorten variable names for plotting purposes
+#
+Datapaper2 <- MultiPoverty
+colnames(Datapaper2)[c(7,9,15,17,19,21)] <- c("Corrup","Democracy","Health.exp.pct","Pop.dens","Rural.pop","Unemp")
+formula <- as.formula(Access.to.electricity + Access.to.drinking.water + Survival.rate +
+                        School.enrollment ~ Corrup + Deaths.in.violence.per.mille + Democracy + Education.expenditure.percentage +
+                        Gini.index + Health.exp.pct + Logistics.performance.index + Pop.dens + Production.index +
+                        Rural.pop + Temperature.change + Unemp)
+#
+# 1 SE-based mvpart tree using 10-fold CV and surrogate split
+#
+set.seed(110)
+poverty.mvpart.1se.fit <- multitree(formula, data = Datapaper2, stand.y = "none",
+                                    tree.xv="1se", tree.xval=10, tree.control = rpart.control(cp=0.01, usesurrogate=2, maxsurrogate = 5))
+
+poverty.mvpart.1se.fit
+plot(poverty.mvpart.1se.fit$Tree)
+text(poverty.mvpart.1se.fit$Tree)
+summary(poverty.mvpart.1se.fit$Tree)
+
+#
+# Note: plot in paper is edited to add node labels and remove extraneous numbers
+#
+
+getTerminal(poverty.mvpart.1se.fit)
+tnodeREEM <- getTerminal(poverty.1se.fit)
+tnodemvpart <- getTerminal(poverty.mvpart.1se.fit)
+table(tnodeREEM$terminal_nodes_where, tnodemvpart$terminal_nodes_where)
+
+tree_world2 <- poverty.mvpart.1se.fit$Tree
+terminal_nodes2 <- with(tree_world2, names(table(where))[table(where) %in%  with(frame, n[var=="<leaf>"])])
+
+world2 <- ne_countries(scale = "medium", returnclass = "sf")
+#
+# Add "years in each terminal node" into the "world" dataset
+world2[  ,c(paste("years in node", terminal_nodes2))] <- NA
+#
+for (i in 1:nrow(world2)) {
+  if (world2$name[i] %in% Datapaper2$Country) {
+    nodes_table2 <- table(tree_world2$where[Datapaper2$Country == world2$name[i]])
+    world2[i ,c(paste("years in node", names(nodes_table2)))] <- nodes_table2
+    world2[i ,c(paste("years in node", setdiff(terminal_nodes2, names(nodes_table2))))] <- 0
+  }
+}
+#
+# Number of years each country falls in each terminal node
+#
+dfworld2=as.data.frame(world2)
+#
+# Show the number of years in which a country falls into a terminal node on the world map, where white means 0 years and
+# gray means not being included in the dataset.
+#
+# Not plotting node 4, since it is only Bhutan 2012-2017
+#
+gglist_treenodes2 <- list(ggplot(data = world2) +
+                            ggtitle("Years in node 1") +
+                            geom_sf(aes(fill = `years in node 5`)) +
+                            scale_fill_gradient(low="white", high="red")+
+                            theme(legend.title = element_blank()),
+                          ggplot(data = world2) +
+                            ggtitle("Years in node 2") +
+                            geom_sf(aes(fill = `years in node 6`)) +
+                            scale_fill_gradient(low="white", high="red")+
+                            theme(legend.title = element_blank()),
+                          ggplot(data = world2) +
+                            ggtitle("Years in node 4") +
+                            geom_sf(aes(fill = `years in node 9`)) +
+                            scale_fill_gradient(low="white", high="red")+
+                            theme(legend.title = element_blank()),
+                          ggplot(data = world2) +
+                            ggtitle("Years in node 5") +
+                            geom_sf(aes(fill = `years in node 11`)) +
+                            scale_fill_gradient(low="white", high="red")+
+                            theme(legend.title = element_blank()),
+                          ggplot(data = world2) +
+                            ggtitle("Years in node 6") +
+                            geom_sf(aes(fill = `years in node 12`)) +
+                            scale_fill_gradient(low="white", high="red")+
+                            theme(legend.title = element_blank()),
+                          ggplot(data = world2) +
+                            ggtitle("Years in node 7") +
+                            geom_sf(aes(fill = `years in node 14`)) +
+                            scale_fill_gradient(low="white", high="red")+
+                            theme(legend.title = element_blank()),
+                          ggplot(data = world2) +
+                            ggtitle("Years in node 8") +
+                            geom_sf(aes(fill = `years in node 15`)) +
+                            scale_fill_gradient(low="white", high="red")+
+                            theme(legend.title = element_blank())                         )
+
+egg::ggarrange(plots=gglist_treenodes2, heights = c(.5, .5, .5, .5))
+
+#
+# Method: Separate REEM trees
+#
+
+library(REEMtree)
+
+formula.elec <- as.formula(Access.to.electricity ~ Corrup + Deaths.in.violence.per.mille + Democracy + Education.expenditure.percentage +
+                             Gini.index + Health.exp.pct + Logistics.performance.index + Pop.dens + Production.index +
+                             Rural.pop + Temperature.change + Unemp)
+formula.water <- as.formula(Access.to.drinking.water ~ Corrup + Deaths.in.violence.per.mille + Democracy + Education.expenditure.percentage +
+                              Gini.index + Health.exp.pct + Logistics.performance.index + Pop.dens + Production.index +
+                              Rural.pop + Temperature.change + Unemp)
+formula.survival <- as.formula(Survival.rate ~ Corrup + Deaths.in.violence.per.mille + Democracy + Education.expenditure.percentage +
+                                 Gini.index + Health.exp.pct + Logistics.performance.index + Pop.dens + Production.index +
+                                 Rural.pop + Temperature.change + Unemp)
+formula.school <- as.formula(School.enrollment ~ Corrup + Deaths.in.violence.per.mille + Democracy + Education.expenditure.percentage +
+                               Gini.index + Health.exp.pct + Logistics.performance.index + Pop.dens + Production.index +
+                               Rural.pop + Temperature.change + Unemp)
+
+set.seed(110)
+poverty.REEM.elec <- REEMtree(formula.elec, random = ~1 | Country, data=Datapaper2)
+library(partykit)
+plot(as.party(poverty.REEM.elec$Tree), type="simple")
+poverty.REEM.water <- REEMtree(formula.water, random = ~1 | Country, data=Datapaper2)
+plot(as.party(poverty.REEM.water$Tree), type="simple")
+poverty.REEM.survival <- REEMtree(formula.survival, random = ~1 | Country, data=Datapaper2)
+plot(as.party(poverty.REEM.survival$Tree), type="simple")
+poverty.REEM.school <- REEMtree(formula.school, random = ~1 | Country, data=Datapaper2)
+plot(as.party(poverty.REEM.school$Tree), type="simple")
